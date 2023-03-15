0ad198be18cd03f19839150517091298bafa3ee8
Andrew Piroli <drewpiroli@gmail.com>
2023-03-15 17:37:50 -0400

    Move Types from xml module to lib

--- sshscan-core/src/lib.rs
+++ sshscan-core/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod xml;
 
 use std::num::ParseIntError;
+use std::str::FromStr;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -11,4 +12,67 @@ pub enum SshScanErr {
     ParseIntError(#[from] ParseIntError),
     #[error("Failed to parse XML")]
     XMLParseFailure(#[from] xmltree::ParseError),
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Host {
+    pub status: HostStatus,
+    pub addr: String,
+    pub port_states: Vec<Description>,
+}
+
+#[derive(Debug, Clone)]
+pub enum HostStatus {
+    Up,
+    Down,
+    Unknown,
+}
+
+impl FromStr for HostStatus {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s.to_ascii_lowercase().as_str() {
+            "up" => Self::Up,
+            "down" => Self::Down,
+            _ => Self::Unknown,
+        })
+    }
+}
+
+impl Default for HostStatus {
+    fn default() -> Self {
+        Self::Unknown
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Description {
+    pub portid: u16,
+    pub state: bool,
+    pub algos: Algos,
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Algos {
+    pub kex_algos: Vec<String>,
+    pub host_key_algos: Vec<String>,
+    pub encryption_algos: Vec<String>,
+    pub mac_algos: Vec<String>,
+    pub compression_algos: Vec<String>,
+}
+
+impl Algos {
+    fn vec_from_xml_key(&mut self, key: &str) -> &mut Vec<String> {
+        match key {
+            "kex_algorithms" => &mut self.kex_algos,
+            "server_host_key_algorithms" => &mut self.host_key_algos,
+            "encryption_algorithms" => &mut self.encryption_algos,
+            "mac_algorithms" => &mut self.mac_algos,
+            "compression_algorithms" => &mut self.compression_algos,
+            _ => {
+                panic!("fixme")
+            }
+        }
+    }
 }
