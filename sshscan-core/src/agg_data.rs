fa0d335bab2e2e13cb87abb1847acc7b5f7d5713
Andrew Piroli <drewpiroli@gmail.com>
2023-04-28 18:35:44 -0400

    Use macro to generate AggregatedData

--- sshscan-core/src/agg_data.rs
+++ sshscan-core/src/agg_data.rs
@@ -1,5 +1,6 @@
 use crate::Host;
 use std::collections::HashMap;
+use paste::paste;
 
 #[derive(Debug, Default, Clone)]
 pub struct AggregatedData<'host> {
@@ -10,56 +11,27 @@ pub struct AggregatedData<'host> {
     compression_algos: HashMap<String, Vec<&'host Host>>,
 }
 
+macro_rules! build {
+    ($host:expr; $hp:expr; $res:expr; {$($nam:expr) +}) => {
+        paste! { $(
+            for $nam in $hp.algos.[<$nam _algos>].iter() {
+                if let Some(existing) = $res.[<$nam _algos>].get_mut($nam) {
+                    existing.push($host);
+                }
+                else {
+                    $res.[<$nam _algos>].insert($nam.to_owned(), vec![$host]);
+                }
+            }
+        )*
+    }; }
+}
+
 impl<'host> AggregatedData<'host> {
     pub fn build_from_hosts(hosts: &'host [Host]) -> Self {
         let mut res = Self::default();
         for host in hosts {
             for host_port in &host.port_states {
-                // kex
-                for kex in host_port.algos.kex_algos.iter() {
-                    if let Some(existing) = res.kex_algos.get_mut(kex) {
-                        existing.push(host);
-                    }
-                    else {
-                        res.kex_algos.insert(kex.to_owned(), vec![host]);
-                    }
-                }
-                // host key
-                for host_key in host_port.algos.host_key_algos.iter() {
-                    if let Some(existing) = res.host_key_algos.get_mut(host_key) {
-                        existing.push(host);
-                    }
-                    else {
-                        res.host_key_algos.insert(host_key.to_owned(), vec![host]);
-                    }
-                }
-                // encryption
-                for encryption in host_port.algos.encryption_algos.iter() {
-                    if let Some(existing) = res.encryption_algos.get_mut(encryption) {
-                        existing.push(host);
-                    }
-                    else {
-                        res.encryption_algos.insert(encryption.to_owned(), vec![host]);
-                    }
-                }
-                // mac
-                for mac in host_port.algos.mac_algos.iter() {
-                    if let Some(existing) = res.mac_algos.get_mut(mac) {
-                        existing.push(host);
-                    }
-                    else {
-                        res.mac_algos.insert(mac.to_owned(), vec![host]);
-                    }
-                }
-                // compression
-                for compression in host_port.algos.compression_algos.iter() {
-                    if let Some(existing) = res.compression_algos.get_mut(compression) {
-                        existing.push(host);
-                    }
-                    else {
-                        res.compression_algos.insert(compression.to_owned(), vec![host]);
-                    }
-                }
+                build!(host; host_port; res; {kex host_key encryption mac compression});
             }
         }
         res
